9cc968e4efadf12db910291e47026cef8ad99d24
rigginsn7 <47797012+rigginsn7@users.noreply.github.com>
2019-10-15 22:02:18 -0400

    Add files via upload

--- TIMER A Blink/timer a blink 6989/main.c
+++ TIMER A Blink/timer a blink 6989/main.c
@@ -1,19 +1,26 @@
-
+/* Author : Nicholas Riggins
+ * Date: October 2019
+ * Timer A blink lab3
+ * board MSP430FR6989
+ * Will blink led 1.0 and 9.7 but instead of using delay cycles
+ * you utilize the processors timer
+ *
+ */
 #include <msp430.h>
 
 int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
-  //P1SEL &= ~BIT0;
- // P9SEL &= ~BIT7;
 
-  P1DIR |= BIT0;
+
+  P1DIR |= BIT0;  // set output direction
   P9DIR |= BIT7;
 
-  P1OUT |= ~BIT0;
+  P1OUT |= ~BIT0; //clears the LED
   P9OUT |= ~BIT7;
 
-  PM5CTL0 &= ~LOCKLPM5;
+  PM5CTL0 &= ~LOCKLPM5;   // Disable the GPIO power-on default high-impedance mode
+                          // to activate previously configured port settings
 
       TA0CCTL0 = CCIE; // Enable interrupt in compare mode
       TA0CTL = TASSEL_2 + MC_1 + ID_2; // SMCLK/4, Up
@@ -24,7 +31,7 @@ int main(void)
       TA1CCR0 = 250000/ 10; // 250000 / 10 = 25000, (10^6 [Hz] / 4) / (25000) = 10Hz
 
 
-  __enable_interrupt();
+  __enable_interrupt();  // enables the interrupts
 
   __bis_SR_register(LPM0 + GIE);
 }
@@ -34,7 +41,7 @@ int main(void)
 __interrupt void TIMER_A0(void)
 
 {
-  P1OUT ^= BIT0;
+  P1OUT ^= BIT0;  // P1.0 toggles
 
 }
 
@@ -42,5 +49,5 @@ __interrupt void TIMER_A0(void)
 __interrupt void TIMER_A1(void)
 
 {
-  P9OUT ^= BIT7;
+  P9OUT ^= BIT7;  // P9.7 toggles
 }
