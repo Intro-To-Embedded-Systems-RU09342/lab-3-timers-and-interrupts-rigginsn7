f0552b1302f0d241ee999be55991a872e47eafcf
rigginsn7 <47797012+rigginsn7@users.noreply.github.com>
2019-10-15 22:02:53 -0400

    Add files via upload

--- Button Interrupt/button interupt 6989/main.c
+++ Button Interrupt/button interupt 6989/main.c
@@ -1,17 +1,25 @@
-
+/* Author : Nicholas Riggins
+ * Date: October 2019
+ * Button Interrupt lab3
+ * board MSP430FR6989
+ * Will blink led 1.0 but instead of running it on a loop
+ * it will wait till the button is pressed to act
+ *
+ */
 #include <msp430.h>
 
 int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
   P1DIR |= BIT0;                            // Set P1.0 to output direction
-  P1IE |=  BIT1;                            // P1.3 interrupt enabled
-  P1IES |= BIT1;                            // P1.3 Hi/lo edge
-  P1REN |= BIT1;                            // Enable Pull Up on SW2 (P1.3)
-  P1IFG &= ~BIT1;                           // P1.3 IFG cleared
-                                            //BIT3 on Port 1 can be used as Switch2
+  P1IE |=  BIT1;                            // P1.1 interrupt enabled
+  P1IES |= BIT1;                            // P1.1 Hi/lo edge
+  P1REN |= BIT1;                            // Enable Pull Up on SW2 (P1.1)
+  P1IFG &= ~BIT1;                           // P1.1 IFG cleared
+                                            //BIT1 on Port 1 can be used as Switch2
 
-  PM5CTL0 &= ~LOCKLPM5;
+  PM5CTL0 &= ~LOCKLPM5;                     // Disable the GPIO power-on default high-impedance mode
+                                            // to activate previously configured port settings
 
   __bis_SR_register(LPM4_bits + GIE);       // Enter LPM4 w/interrupt
 }
@@ -24,5 +32,5 @@ __interrupt void Port_1(void)
 #endif
 {
   P1OUT ^= BIT0;                            // P1.0 = toggle
-  P1IFG &= ~BIT1;                           // P1.3 IFG cleared
+  P1IFG &= ~BIT1;                           // P1.1 IFG cleared
 }
